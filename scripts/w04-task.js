d74c0760602cb284058ec3f5bc7fd3dcf2dcdafe
TheeJStar <kingoflightj@gmail.com>
2024-02-04 06:56:23 -0800

    fixed some misspellings

--- scripts/w04-task.js
+++ scripts/w04-task.js
@@ -1,7 +1,7 @@
 /* LESSON 3 - Programming Tasks */
 
 /* Profile Object  */
-let myProject = {
+let myProfile = {
     name: "Joel Ayvazyan",
     photo: "./images/myProfilePicture.jpg",
     favoriteFoods: [
@@ -20,32 +20,30 @@ let myProject = {
     placesLived: []
 };
 
-
-
 /* Populate Profile Object with placesLive objects */
-myProject.placesLived.push({place: "Yerevan, Armenia", length: "19 years"});
+myProfile.placesLived.push({place: "Yerevan, Armenia", length: "19 years"});
 
 /* DOM Manipulation - Output */
 
 /* Name */
-document.querySelector("#name").textContent = myProject.name;
+document.querySelector("#name").textContent = myProfile.name;
 /* Photo with attributes */
-document.querySelector("#photo").src = myProject.photo;
-document.querySelector("#photo").alt = myProject.name;
+document.querySelector("#photo").src = myProfile.photo;
+document.querySelector("#photo").alt = myProfile.name;
 /* Favorite Foods List*/
-myProject.favoriteFoods.forEach(food => {
+myProfile.favoriteFoods.forEach(food => {
     let li = document.createElement("li");
     li.textContent = food;
     document.querySelector("#favorite-foods").appendChild(li);
 })
 /* Hobbies List */
-myProject.hobbies.forEach(hobby => {
+myProfile.hobbies.forEach(hobby => {
     let li = document.createElement("li");
     li.textContent = hobby;
     document.querySelector("#hobbies").appendChild(li);
 })
 /* Places Lived DataList */
-myProject.placesLived.forEach(object => {
+myProfile.placesLived.forEach(object => {
     let dt = document.createElement("dt");
     dt.textContent = object.place;
     let dd = document.createElement("dd");
